b6e1db9fee7faa9d5e56923e7816413cc8f5c0b4
Bethesdanaa <105776817+Bethesdanaa@users.noreply.github.com>
2023-02-12 16:02:53 -0800

    Add files via upload
    ##beginner programmer

---
+++ multiplication table.py
@@ -0,0 +1,11 @@
+# Multiplication table 
+num = 1
+# to take input from user
+num = int(input('display multiplication table of'))
+for i in range (1,11):
+    print (num, 'x', i, '=', num*i)
+    
+    
+        
+        
+        
